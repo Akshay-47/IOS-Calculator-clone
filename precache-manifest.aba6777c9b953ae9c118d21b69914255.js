2158ca9996006b79ad9a0c98e8ce04386c2e061e
Akshay.lr <akshay.lr@tadigital.com>
2019-10-29 19:05:22 +0530

    Updates

---
+++ precache-manifest.aba6777c9b953ae9c118d21b69914255.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea3b18e3d8490bd389b880727e264a93",
+    "url": "/IOS-Calculator-clone/index.html"
+  },
+  {
+    "revision": "d3dd053eaf445924d9b1",
+    "url": "/IOS-Calculator-clone/static/css/main.c8ec594a.chunk.css"
+  },
+  {
+    "revision": "b275930602cb3de73d39",
+    "url": "/IOS-Calculator-clone/static/js/2.cc9bbc03.chunk.js"
+  },
+  {
+    "revision": "d3dd053eaf445924d9b1",
+    "url": "/IOS-Calculator-clone/static/js/main.a84f9b6f.chunk.js"
+  },
+  {
+    "revision": "687373f37b7926fc666c",
+    "url": "/IOS-Calculator-clone/static/js/runtime-main.35099765.js"
+  }
+]);
